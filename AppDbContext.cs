1e3fea7bc5d8c12a7394105deb4825eca60f0e37
Elshewimy <elshewimy6677@gmail.com>
2025-08-12 17:41:22 +0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFSchoolLab.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace EFSchoolLab
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students => Set<Student>();
+
+        public DbSet<Course> Courses => Set<Course>();
+
+        public DbSet<Instructor> Instructors => Set<Instructor>();
+        public DbSet<Department> Departments => Set<Department>();
+        public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(SchoolContext).Assembly);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using EFSchoolLab;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+
+using var context = new SchoolContext();
+
+var students = context.Students.ToList();
+foreach (var s in students)
+{
+    Console.WriteLine($"{s.Name} - {s.StudentId}");
+}
+
+var coursesWithInstructors = context.Courses
+    .Include(c => c.Enrollments)
+    .ThenInclude(e => e.Student)
+    .Select(c => new
+    {
+        c.Title,
+        Instructors = c.Enrollments.Select(e => e.Student.Name)
+    })
+    .ToList();
+
+foreach (var c in coursesWithInstructors)
+{
+    Console.WriteLine($"{c.Title}: {string.Join(", ", c.Instructors)}");
+}
+
+var enrollments =
+    from e in context.Enrollments
+    join s in context.Students on e.StudentId equals s.StudentId
+    join c in context.Courses on e.CourseId equals c.CourseId
+    select new
+    {
+        StudentName = s.Name,
+        CourseTitle = c.Title,
+        e.Grade
+    };
+
+foreach (var e in enrollments)
+{
+    Console.WriteLine($"{e.StudentName} - {e.CourseTitle} - Grade: {e.Grade}");
+}
+
+
